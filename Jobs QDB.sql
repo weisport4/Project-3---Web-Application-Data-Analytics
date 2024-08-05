7d205fedabfbefb726e7454df7f46f233223641f
weisport4 <weisport4@gmail.com>
2024-08-05 11:46:16 -0500

    added datafiles jobs creation Sql and project proposal

---
+++ Jobs QDB.sql
@@ -0,0 +1,54 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/T7GRz5
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "jobs" (
+    "fips" VARCHAR(05)   NOT NULL,
+    "state" VARCHAR(2)   NOT NULL,
+    "county" VARCHAR(40)   NOT NULL,
+    "pctemp_agriculture" DECIMAL   NOT NULL,
+    "pctemp_mining" DECIMAL   NOT NULL,
+    "pctemp_construction" DECIMAL   NOT NULL,
+    "pctemp_manufacturing" DECIMAL   NOT NULL,
+    "pctemp_trade" DECIMAL   NOT NULL,
+    "pctemp_trans" DECIMAL   NOT NULL,
+    "pctemp_information" DECIMAL   NOT NULL,
+    "pctemp_fire" DECIMAL   NOT NULL,
+    "pctemp_services" DECIMAL   NOT NULL,
+    "pctemp_government" DECIMAL   NOT NULL,
+    "num_civ_employed" int   NOT NULL,
+    "last_update" timestamp  DEFAULT Localtimestamp NOT NULL,
+    CONSTRAINT "pk_jobs" PRIMARY KEY (
+        "fips"
+     )
+);
+
+CREATE TABLE "unemloyment" (
+    "fips" VARCHAR(05)   NOT NULL,
+    "year" VARCHAR(04)   NOT NULL,
+    "unemp_rate" DECIMAL   NOT NULL,
+    "num_unemployed" INT   NOT NULL,
+    "last_update" timestamp  DEFAULT Localtimestamp NOT NULL,
+    CONSTRAINT "pk_unemloyment" PRIMARY KEY (
+        "fips"
+     )
+);
+
+CREATE TABLE "employment" (
+    "fips" VARCHAR(05)   NOT NULL,
+    "year" VARCHAR(04)   NOT NULL,
+    "num_civ_labor_force" INT   NOT NULL,
+    "num_employed" INT   NOT NULL,
+    "last_update" timestamp  DEFAULT Localtimestamp NOT NULL,
+    CONSTRAINT "pk_employment" PRIMARY KEY (
+        "fips"
+     )
+);
+
+ALTER TABLE "unemloyment" ADD CONSTRAINT "fk_unemloyment_fips" FOREIGN KEY("fips")
+REFERENCES "jobs" ("fips");
+
+ALTER TABLE "employment" ADD CONSTRAINT "fk_employment_fips" FOREIGN KEY("fips")
+REFERENCES "jobs" ("fips");
+
