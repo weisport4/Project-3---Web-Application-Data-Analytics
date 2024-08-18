81e8c31d042dcd8f24854058c5f567895642da01
Lauz11 <laurazapatadiaz315@gmail.com>
2024-08-18 12:53:11 -0500

    Merge branch 'main' after resolving conflicts

---
+++ select_sql.sql
@@ -0,0 +1,32 @@
+﻿-- The UNION ALL including the table_name as a column prior to the count will give us a list of all tables and their respective counts
+
+SELECT 
+    'jobs' AS table_name, 
+    COUNT(*) AS table_count 
+FROM 
+    jobs
+UNION ALL
+SELECT 
+    'unemployment' AS table_name, 
+    COUNT(*) AS table_count 
+FROM 
+    unemployment
+UNION ALL
+SELECT 
+    'employment' AS table_name, 
+    COUNT(*) AS table_count 
+FROM 
+    employment
+UNION ALL
+SELECT 
+    'income' AS table_name, 
+    COUNT(*) AS table_count 
+FROM 
+    income
+UNION ALL
+SELECT 
+    'state' AS table_name, 
+    COUNT(*) AS table_count 
+FROM 
+    state
+;
